e8e8d26a691c458fbc4c34af130156814f3a3a15
Saif <saifmomani7899@gmail.com>
2026-02-28 12:35:25 -0500

    misc

--- player.gd
+++ player.gd
@@ -24,6 +24,6 @@ func _physics_process(delta: float) -> void:
 
 		if (Input.is_action_just_pressed("ui_select") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
 			velocity.y = jumpHeight 
-
+	
 	velocity.x = moveDirection * walkSpeed
 	move_and_slide()
