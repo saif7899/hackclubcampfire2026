5a24619b9b819362f176b1640acf161730140c19
Saif <saifmomani7899@gmail.com>
2026-02-28 12:24:58 -0500

    jump

--- player.gd
+++ player.gd
@@ -4,11 +4,11 @@ var state: States = States.base
 
 @export var walkSpeed = 100
 @export var digSpeed = 150
-@export var gravity = 110
+@export var gravity = 160
 var moveDirection = Vector2.RIGHT
 var inputDir = Vector2.ZERO
 
-@export var jumpHeight = 100
+@export var jumpHeight = -100
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -21,8 +21,9 @@ func _physics_process(delta: float) -> void:
 			$Sprite2D.flip_h = true
 		else:
 			$Sprite2D.flip_h = false
-		if Input.is_action_just_pressed("ui_select") and is_on_floor():
-			velocity.y = jumpHeight
-			
+
+		if (Input.is_action_just_pressed("ui_select") or Input.is_action_just_pressed("ui_up")) and is_on_floor():
+			velocity.y = jumpHeight 
+
 	velocity.x = moveDirection * walkSpeed
 	move_and_slide()
